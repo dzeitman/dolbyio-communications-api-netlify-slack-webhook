3e0e03357639858efa346e41c8f1fc4d4b36f435
Dan Zeitman <dvpweb@gmail.com>
2021-11-22 17:51:35 -0800

    Update dolbyio-netlify-webhook.js
      const fetch = require("node-fetch"); inside code block

--- functions/dolbyio-netlify-webhook.js
+++ functions/dolbyio-netlify-webhook.js
@@ -20,11 +20,11 @@
 
 // const querystring = require("querystring");
 // const fetch = require("node-fetch");
-import { fetch } from 'node-fetch';
+// import { fetch } from 'node-fetch';
 
 //  Our FAS
 exports.handler = async (event, context) => {
-
+  const fetch = require("node-fetch");
   // Dolby.io logo image
   const logoImage = "https://avatars.slack-edge.com/2021-07-28/2316131338342_1f6488351e04582ba704_512.jpg"
 
